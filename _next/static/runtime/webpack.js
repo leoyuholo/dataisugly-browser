0c20080077d04f4a1dd33ee395fa6133250809b6
Leo Yu Ho, Lo <leoyuholo@gmail.com>
2020-08-09 15:42:39 +0000

    add taxonomies and descriptions

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b712d28c677f6db781a9";
+/******/ 	var hotCurrentHash = "433432a387c1fccdc3a1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
