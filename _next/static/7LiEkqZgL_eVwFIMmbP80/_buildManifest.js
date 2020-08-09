57f793f61db3f29108bd4e778a8d17da3b3542de
Leo Yu Ho, Lo <leoyuholo@gmail.com>
2020-08-09 16:13:24 +0000

    upgrade dependencies

--- _next/static/7LiEkqZgL_eVwFIMmbP80/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002F29107295.5ca7b6214301d7164f6e.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
