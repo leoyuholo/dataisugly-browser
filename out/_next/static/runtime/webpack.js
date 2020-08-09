2f7b0319001257ae440f148e1f5d2523684e4dbf
Leo Yu Ho, Lo <leoyuholo@gmail.com>
2020-08-09 16:13:24 +0000

    upgrade dependencies

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "433432a387c1fccdc3a1";
+/******/ 	var hotCurrentHash = "cf3b1addf97e3abff3e3";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
