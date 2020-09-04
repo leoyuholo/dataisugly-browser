2f11438dba056c1755c468667d65fcf1eb1928a8
Leo Yu Ho, Lo <leoyuholo@gmail.com>
2020-09-04 13:44:38 +0000

    fix favicon

---
+++ _next/static/Avhu6bULm23t-l1l6cHyp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-eba3c708cfe70b13933c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-49a0c38c4f0092a21e9d.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
